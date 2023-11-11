1418a7f6911b05347a1e17430fc5eab4e507c280
JamieChen007 <107536976+JamieChen007@users.noreply.github.com>
2023-11-11 23:41:37 +1100

    feat: update modal width css

--- src/components/UI/TipsModal/TipsModal.js
+++ src/components/UI/TipsModal/TipsModal.js
@@ -5,7 +5,7 @@ import Button from "../Button/";
 const TipsModal = ({ tips, onTipsConfirmBtnClick }) => {
   return (
     <Backdrop>
-      <Card className="w-[500px] h-[200px] bg-white absolute m-auto top-0 bottom-0 right-0 left-0 flex flex-col p-2">
+      <Card className="w-[500px] h-[200px] max-xs:max-w-[250px] bg-white absolute m-auto top-0 bottom-0 right-0 left-0 flex flex-col p-2">
         <div className="h-[140px] text-2xl flex items-center justify-center">
           <p>{tips}</p>
         </div>
